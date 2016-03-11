8d557e3e1cf4e074e6134b913da75a8977ae4bb0
Jorge Vidal <jvidal@syspixel.com>
2016-03-11 13:14:45 +0100

    vim-puppet repo changed by other more updated

--- manifests/params.pp
+++ manifests/params.pp
@@ -18,7 +18,7 @@ class vim_tuning::params {
   $user            = 'root'
   $homedir         = "/${user}"
   $plugins_ensure  = 'present'
-  $plugins         = ['ctrlpvim/ctrlp.vim', 'scrooloose/nerdtree', 'rodjek/vim-puppet',
+  $plugins         = ['ctrlpvim/ctrlp.vim', 'scrooloose/nerdtree', 'voxpupuli/vim-puppet',
                       'scrooloose/syntastic', 'godlygeek/tabular', 'tomtom/tlib_vim',
                       'MarcWeber/vim-addon-mw-utils', 'vim-airline/vim-airline', 'vim-airline/vim-airline-themes',
                       'garbas/vim-snipmate', 'honza/vim-snippets', 'pld-linux/vim-syntax-vcl', 'sclo/haproxy.vim']
